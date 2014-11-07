2f6614fd517eec0d0f6c621b8eba2a08615ccc91
midhun7416 <midhun.nitw@gmail.com>
2014-11-06 19:43:19 -0600

    hi

--- OFClassifier.py
+++ OFClassifier.py
@@ -3,39 +3,58 @@ import socketserver
 import netifaces as ni
 import codecs
 import json
-import requests
 
+globalTypeCounter = {}
+perSwitchTypeCounter = {}
+
+class OFMsgClassifier():
+
+    def __init__(self):
+        pass
+
+    def classify(self, pkt):
+        pass
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
 
     def handle(self):
+        globals()
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(10200)
-        #self.data = self.request.recv(1024)
+        switch = 'switch'
         #print ("{} wrote:",format(self.client_address[0]))
         print (self.data)
         reader = codecs.getreader("utf-8")
         data = self.data.decode('utf-8')
+        print(data)
         j = json.loads(data)
-        # just send back the same data, but upper-cased
-        #self.request.sendall(self.data.upper())
+        for k in j:
+            print(k,j[k])
+        j = json.loads(data)
+
+        if j[switch] not in perSwitchTypeCounter:
+            perSwitchTypeCounter[j[switch]] = {}
+        for k in j:
+            if k != 'switch':
+                if k not in globalTypeCounter:
+                    globalTypeCounter[k] = 1
+                else:
+                    globalTypeCounter[k] += 1
+                if k not in perSwitchTypeCounter[j[switch]]:
+                    perSwitchTypeCounter[j[switch]][k] = 1
+                else:
+                    perSwitchTypeCounter[j[switch]][k] += 1
+        print(globalTypeCounter)
+        print(perSwitchTypeCounter)
+
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "localhost", 9998
     # Create the server, binding to localhost on port 9999
-    ni.ifaddresses('eth0')
-    ip = ni.ifaddresses('eth0')[2][0]['addr']
+    ni.ifaddresses('wlan0')
+    ip = ni.ifaddresses('wlan0')[2][0]['addr']
     print (ip)  # should print "192.168.100.37"
     server = socketserver.TCPServer((ip, PORT), MyTCPHandler)
-
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
-
